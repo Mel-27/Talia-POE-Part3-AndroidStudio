c37578fcf0b6ef5fc9c56c8d64470dcf3c62811c
Ayushijagg <st10452981@vcconnect.edu.za>
2026-05-24 16:51:16 +0200

    Implemented supabase client provider

---
+++ app/src/main/java/com/gia/poe_demo/SupabaseClientProvider.kt
@@ -0,0 +1,22 @@
+package com.gia.poe_demo
+
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.storage.Storage
+
+
+object SupabaseClientProvider {
+
+
+    private const val SUPABASE_URL = "https://tgxsycobmoakgomunvou.supabase.co"
+    private const val SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRneHN5Y29ibW9ha2dvbXVudm91Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2Mjg0NjQsImV4cCI6MjA5NDIwNDQ2NH0.d4BAp_NkzYGooDLBfVYFjdaSzERfBVuvgj9dwJx1kIE"
+
+    val client = createSupabaseClient(
+        supabaseUrl = SUPABASE_URL,
+        supabaseKey = SUPABASE_ANON_KEY
+    ) {
+        install(Storage)
+        install(Postgrest)
+    }
+}
+
